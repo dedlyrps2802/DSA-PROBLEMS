ccceb010495cbdc0476a7e29c4d780e60529a3d1
dedlyrps2802 <singhrudra8957@gmail.com>
2025-10-05 19:24:00 +0530

    Time: 62 ms (38.58%) | Memory: 47.8 MB (48.98%) - LeetSync

---
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,50 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        
+       int sum = 0;
+        for(int i = 0; i< nums.length; i++){
+          sum += nums[i];
+        }
+
+        if(sum % 2 != 0){
+            return false;
+        }
+
+        else{
+
+            return subsetSum(nums , sum/2 );
+        }
+
+    }
+
+    boolean subsetSum(int[] arr ,int  sum){
+        int n = arr.length;
+        boolean t[][] = new boolean[n+1][sum+1];
+
+        for(int i = 0; i<n+1; i++){
+            for(int j = 0; j< sum+1; j++){
+                if(j==0){
+                    t[i][j] = true;
+                }
+                else if(i==0 && j!=0){
+                    t[i][j] = false;
+                }
+            }
+        }
+
+        for(int i = 1 ; i < n+1; i++){
+            for(int j = 1 ; j<sum+1 ; j++){
+                if(arr[i-1] > j){
+                    t[i][j] = t[i-1][j];
+
+                }
+
+                else{
+                    t[i][j] = t[i-1][j- arr[i-1]] || t[i-1][j];
+                }
+            }
+        }
+
+        return t[n][sum];
+    }
+}
